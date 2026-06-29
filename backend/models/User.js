268c940dd750c5bce7f8243cf28da8071bcb703a
Milton Franco Ruiz <94766039+miltonfruiz@users.noreply.github.com>
2026-06-29 03:48:34 -0300

    Add backend/models/User.js

---
+++ backend/models/User.js
@@ -0,0 +1,24 @@
+```javascript
+const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+
+const userSchema = new mongoose.Schema({
+  email: { type: String, unique: true, required: true },
+  password: { type: String, required: true },
+  rol: { type: String, enum: ['admin', 'user'], default: 'user' }
+});
+
+userSchema.methods.comparePassword = async function(password) {
+  return await bcrypt.compare(password, this.password);
+};
+
+userSchema.pre('save', async function(next) {
+  if (!this.isModified('password')) return next();
+  this.password = await bcrypt.hash(this.password, 10);
+  next();
+});
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
+```
